e7f2cd7822689cabf1e9883811f4c5b81bc90355
Leo <leo.hilsheimer@gmail.com>
2023-08-07 17:17:58 +0100

    scroll fx on landing page

--- app/views/Landing.tsx
+++ app/views/Landing.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import classes from './landing.module.css';
 
 export const Landing: React.FC = () => {
+  const leave = useRef<HTMLDivElement>(null);
+
+  const [scrollY, setScrollY] = useState(0);
+  const [play, setPlay] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    if (leave.current) {
+      if ((scrollY ?? 0) > window.scrollY) {
+        // Scrolling up
+        setPlay(0);
+      } else if ((scrollY ?? 0) < window.scrollY) {
+        // Scrolling down
+        setPlay(1)
+      }
+    }
+
+    setScrollY(window.scrollY)
+  }, [scrollY]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <section className={classes.container}>
-      <div className={classes.bg}>
-        <div></div>
-        <div className={classes.layer2}></div>
-      </div>
+      <div className={classes.bg}></div>
+      <div className={classes.bgSecondary} ref={leave} data-play={play}></div>
       <img className={classes.logo} src="/img/logo.png" alt="the tree temple logo" />
     </section>
   )
 }
   
-  export default Landing;
+export default Landing;
   
